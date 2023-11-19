df3fcd7ae2987b9a9a324affaf82fbec293c8ffb
LEEBONGHAK <dlqhdgkr456@gmail.com>
2023-11-19 22:53:53 +0900

    7-10 PRG - Post/Redirect/Get

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -58,6 +58,10 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    /**
+     * @ModelAttribute("item") Item item
+     * model.addAttribute("item", item); 자동 추가
+     */
 //    @PostMapping("/add")
     public String addItemV2(@ModelAttribute("item") Item item, Model model) {
 
@@ -67,15 +71,40 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+    /**
+     * @ModelAttribute name 생략 가능
+     * model.addAttribute(item); 자동 추가, 생략 가능
+     * 생략시 model에 저장되는 name은 클래스명 첫글자만 소문자로 등록 Item -> item
+     */
+//    @PostMapping("/add")
     public String addItemV3(@ModelAttribute Item item, Model model) {
-
 
         itemRepository.save(item);
         //Item -> item -> model.addAttribute("item", item);
 //        model.addAttribute("item", item);
 
         return "basic/item";
+    }
+
+    /**
+     * @ModelAttribute 자체 생략 가능
+     * model.addAttribute(item) 자동 추가
+     */
+//    @PostMapping("/add")
+    public String addItemV4(Item item) {
+
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    /**
+     * PRG - Post/Redirect/Get
+     */
+    @PostMapping("/add")
+    public String addItemV5(Item item) {
+
+        itemRepository.save(item);
+        return "redirect:/basic/items/" + item.getId();
     }
 
     @GetMapping("/{itemId}/edit")
